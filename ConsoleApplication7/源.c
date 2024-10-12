4bcc479c6c8978024ab8bb963a070b93465f375b
zzhzhengzehong <2297378225@qq.com>
2024-10-12 14:35:33 +0800

    Add files via upload

---
+++ ConsoleApplication7/源.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int n, a, b, d, z;
+	int e, f, g, h, i, j;
+	scanf("%d", &n);
+	scanf("%d %d", &e, &f);
+	scanf("%d %d", &g, &h);
+	scanf("%d %d", &i, &j);
+	a = n / e; f = n % e == 0 ? a: a+ 1; a = e * f;
+		b = n / g; h = n % g == 0 ? b: b+ 1; b = g * h;
+		d = n / i; j = n % i == 0 ? d : d + 1; d = i * j;
+	
+	
+	if (a < b && a < d) z = a;
+	if (b < a && b < d) z = b;
+	if (d < a && d < b)z = d;
+	printf("%d", z);
+	return z;
+}
