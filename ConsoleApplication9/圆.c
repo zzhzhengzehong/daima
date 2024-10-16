df5665edce2dd16044dce58e001bfa982545450c
zzhzhengzehong <2297378225@qq.com>
2024-10-16 13:08:34 +0800

    Add files via upload

---
+++ ConsoleApplication9/圆.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	double r=0, h=0, pi = 3.14;
+//	printf("请输入圆半径：");
+//	scanf("%lf", &r);
+//	printf("请输入圆柱高：");
+//	scanf("%lf",&h);
+//	printf("圆周长：%lf\n", 2*pi*r);
+//	printf("圆面积：%lf\n", pi *r*r);
+//	printf("圆球表面积：%lf\n", 4*pi*r*r);
+//	printf("圆球体积：%lf\n", 4/3* pi * r*r*r);
+//	printf("圆柱体积：%lf\n", pi*r*r*h);
+//	return 0;
+//}
