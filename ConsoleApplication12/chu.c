484ef63d1127353989fa2334839558f27a4a3db5
zzhzhengzehong <2297378225@qq.com>
2024-11-02 23:16:56 +0800

    Add files via upload

---
+++ ConsoleApplication12/chu.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	int i = 0;
+//	printf("请输入被除数:");
+//	scanf("%d", &a);
+//	printf("请输入除数:");
+//	scanf("%d",&b);
+//	
+//	
+//	while (a>=b)
+//	{
+//		a -= b;
+//		i++;
+//	}
+//	printf("商：%d\n", i);
+//	printf("余数：%d\n", a);
+//	return 0;
+//}
---
+++ ConsoleApplication12/huiwen.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int ori,number=0;
+//	int rev = 0, temp = 0;
+//	scanf("%d",&number );
+//	ori = number;
+//	while ( number> 0)
+//	{
+//		temp = number % 10;
+//		number = number / 10;
+//		rev = rev * 10 + temp;
+//	}
+//	if (rev == ori) {
+//		printf("yes");
+//	}
+//	else {
+//		printf("no");
+//	}
+//	
+//	return 0;
+//}
